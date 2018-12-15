9dda58d8496bcddd57626231e40590b4453f7f98
前端开发部 - 陈骥 <chenji@jiguang.cn>
2018-12-15 12:19:52 +0800

    1-5 项目底部导航开发

--- App.js
+++ App.js
@@ -6,25 +6,62 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, Image, SafeAreaView } from 'react-native';
+import TabNavigator from 'react-native-tab-navigator';
 
 type Props = {};
 export default class App extends Component<Props> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedTab: 'tb_popular'
+    };
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_popular'}
+            selectedTitleStyle={{ color: 'red' }}
+            title="最热"
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, { tintColor: 'red' }]} source={require('./res/images/ic_polular.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'tb_popular' })}>
+            <View style={{ backgroundColor: 'red', flex: 1 }}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_trending'}
+            title="趋势"
+            selectedTitleStyle={{ color: 'yellow' }}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, { tintColor: 'yellow' }]} source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+            <View style={{ backgroundColor: 'yellow', flex: 1 }}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_favorite'}
+            title="收藏"
+            selectedTitleStyle={{ color: 'green' }}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_favorite.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, { tintColor: 'green' }]} source={require('./res/images/ic_favorite.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_favorite' })}>
+            <View style={{ backgroundColor: 'green', flex: 1 }}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_my'}
+            title="我的"
+            selectedTitleStyle={{ color: 'blue' }}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image, { tintColor: 'blue' }]} source={require('./res/images/ic_my.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+            <View style={{ backgroundColor: 'blue', flex: 1 }}></View>
+          </TabNavigator.Item>
+        </TabNavigator>
+      </SafeAreaView>
     );
   }
 }
@@ -32,18 +69,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  image: {
+    height: 22,
+    width: 22
+  }
 });
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 #react-native实战
-##开发准备
+## 第一章 开发准备
+### 1-5项目底部导航开发
+react-native-tab-navigate使用
+> png图片可以在renderIcon中通过tintColor进行着色,这个是ios的着色机制
